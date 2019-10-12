f769d8dd447a882f06625c18a49f1df143d5574d
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2019-10-12 21:40:36 +1100

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,66 @@
 <!DOCTYPE html>
+<?php
+session_start();
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+$bucketName = 'ccptvapp';
+$IAM_KEY = 'AKIAWF2GCE4OZYJEWXG7';
+$IAM_SECRET = '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';
+if (isset($_POST["submit"])) {
+    
+if ($_FILES["file"]["error"] > 0)
+{
+    echo "Error: " . $_FILES["file"]["error"] . "<br />";
+}
+else
+{
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Stored in: " . $_FILES["file"]["tmp_name"];
+}
+}
+
+try {
+    // You may need to change the region. It will say in the URL when the bucket is open
+    // and on creation.
+    $s3 = S3Client::factory(
+    array(
+    'credentials' => array(
+    'key' => $IAM_KEY,
+    'secret' => $IAM_SECRET
+    ),
+    'version' => 'latest',
+    'region'  => 'ap-southeast-2'
+        )
+    );
+} catch (Exception $e) {
+    // We use a die, so if this fails. It stops here. Typically this is a REST call so this would
+    // return a json object.
+    die("Error: " . $e->getMessage());
+}
+
+try {
+    // Uploaded:
+    $file = $_FILES["fileToUpload"]['tmp_name'];
+    $s3->putObject(
+        array(
+            'Bucket'=>$bucketName,
+            'Key' =>  $keyName,
+            'SourceFile' => $file,
+            'StorageClass' => 'REDUCED_REDUNDANCY'
+        )
+        );
+} catch (S3Exception $e) {
+    die('Error:' . $e->getMessage());
+} catch (Exception $e) {
+    die('Error:' . $e->getMessage());
+}
+?>
 <html lang="en">
 
 <head>
@@ -7,16 +69,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
-  <title>Bare - Start Bootstrap Template</title>
-
+  <title>GTFS Shapes Mapper</title>
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
 </head>
-
 <body>
-
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
@@ -44,7 +101,6 @@
       </div>
     </div>
   </nav>
-
   <!-- Page Content -->
   <div class="container">
     <div class="row">
@@ -58,11 +114,15 @@
       </div>
     </div>
   </div>
-
+  <form action="index.php" method="post"
+	enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file" /> 
+<br />
+<input type="submit" name="submit" value="Submit" />
+</form>
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.slim.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
 </body>
-
 </html>
