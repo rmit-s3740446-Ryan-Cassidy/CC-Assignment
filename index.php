3b024333442286c66ef79daab808970e20deaf0c
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2019-10-13 17:16:34 +1100

    Add files via upload

--- index.php
+++ index.php
@@ -1,158 +1,141 @@
+<!DOCTYPE html>
 <?php
-	session_start();
+session_start();
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+require 'vendor/autoload.php';
 
-	// Check for account creation
-	if ($_SESSION['success'] == true){
-		echo '<div class="alert alert-success" name="accountSuccess">';
-		echo '<strong>Success!</strong> Account created.';
-		echo '</div>';
-		unset($_SESSION['success']);
-	}
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+$bucketName = 'ccptvapp';
+$IAM_KEY = 'AKIAWF2GCE4OTQ77KZDT';
+$IAM_SECRET = 'Dt1o3ROc6db6bZSbleufErXUVh8xU9vYUTIx4ulm';
+if (isset($_POST["submit"])) {
+   
+    //File info success TESTING PLEASE REMOVE LATER OR REPLACE
+if ($_FILES["file"]["error"] > 0)
+{
+    echo "Error: " . $_FILES["file"]["error"] . "<br />";
+}
+else
+{
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Stored in: " . $_FILES["file"]["tmp_name"];
+}
 
-	// Check for password reset
-	if ($_SESSION['reset_success'] == true){
-		echo '<div class="alert alert-success" name="passSuccess">';
-		echo '<strong>Success!</strong> Password was updated successfully.';
-		echo '</div>';
-		unset($_SESSION['reset_success']);
-	}
 
-	// Login validation
-	if (isset($_POST['email']) && isset($_POST['password'])) {
-		// Check if any fields are empty
-		if (empty($_POST['email']) || empty($_POST['password'])) {
-			include('header.php');
-			echo '<div class="alert alert-warning" name="emptyWarn">';
-			echo '<strong>Warning!</strong> Email address or password cannot be empty.';
-			echo '</div>';
-		} else {
-			$valid = false;
-			// Check datastore for matching login
-			include('schema.php');
-			foreach($obj_account_store->fetchAll() as $obj_account) {
-				if ($_POST['email'] == $obj_account->email && $_POST['password'] == $obj_account->password){
-					$_SESSION['user_id'] = $obj_account->user_id;
-					$valid = true;
-				}
-			}
-			// Check if the credentials entered are valid
-			if ($valid) {
-				$_SESSION['logged_in'] = true;
-				header("Location: account/index.php");
-			} else {
-				include('header.php');
-				echo '<div class="alert alert-warning" name="invalidWarn">';
-				echo '<strong>Warning!</strong> Invalid login details.';
-				echo '</div>';
-			}
-		}
-	// Default page layout
-	} else {
-		include('header.php');
-	}
+try {
+    // You may need to change the region. It will say in the URL when the bucket is open
+    // and on creation.
+    $s3 = S3Client::factory(
+    array(
+    'credentials' => array(
+    'key' => $IAM_KEY,
+    'secret' => $IAM_SECRET
+    ),
+    'version' => 'latest',
+    'region'  => 'ap-southeast-2'
+        )
+    );
+} catch (Exception $e) {
+    // We use a die, so if this fails. It stops here. Typically this is a REST call so this would
+    // return a json object.
+    die("Error: " . $e->getMessage());
+}
+
+// For this, I would generate a unqiue random string for the key name. But you can do whatever.
+$keyName = 'key/' . basename($_FILES['file']['tmp_name']);
+$pathInS3 = 'https://s3.ap-southeast-2.amazonaws.com/' . $bucketName . '/' . $keyName;
+
+try {
+    // Uploaded:
+    $file = $_FILES['file']['tmp_name'];
+    $s3->putObject(
+        array(
+            'Bucket'=>$bucketName,
+            'Key' =>  $keyName,
+            'SourceFile' => $file,
+            'StorageClass' => 'REDUCED_REDUNDANCY'
+        )
+        );
+} catch (S3Exception $e) {
+    die('Error:' . $e->getMessage());
+} catch (Exception $e) {
+    die('Error:' . $e->getMessage());
+}
+}
 ?>
+<html lang="en">
 
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item active" aria-current="page">Home</li>
-  </ol>
-</nav>
+<head>
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 bg-light">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
-		<h1 class="display-4 font-weight-normal text-center">Login</h1>
-		<div class="card">
-			<div class="card-body">
-				<form action="index.php" method="post">
-					<div class="form-group">
-						<label for="email">Email</label>
-						<input type="text" class="form-control" name="email">
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input type="password" class="form-control" name="password">
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Submit" name="submitBtn" class="btn btn-primary float-right">
-					</div>
-					<div class="form-group">
-						<a href="account/forgot_password.php" class="card-link" name="forgotLink">Forgot password?</a>
-						<a href="account/register.php" class="card-link" name="registerLink">Register</a>
-					</div>
-				</form>
-			</div>
-		</div>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>GTFS Shapes Mapper</title>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Cloud Computing</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Stats
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="testmap.php">Map</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sample.php">Sample</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="product-device shadow-sm d-none d-md-block"></div>
-    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-</div>
-
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-        <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </nav>
+  <!-- Page Content -->
+  <div class="col-md-5 p-lg-5 mx-auto my-5">
+  <div class="container">
+  <div class="card">
+  <div class="card-body">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">Coordinates Mapping Application</h1>
+        <p class="lead">Cloud Computing Semester 2 2019</p>
+        <p class="lead">Ryan Cassidy & Vineet Bugtani</p>
+        <ul class="list-unstyled">
+          <li>Please select a csv coordinates file</li>
+          <form action="index.php" method="post"
+	enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file" /> 
+<br />
+<input type="submit" name="submit" value="Submit" />
+</form>
+        </ul>
+      </div>
     </div>
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
-</div>
-
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
-    <div class="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-        <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-</div>
-
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-</div>
-
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-</div>
-
-<?php include('footer.php'); ?>
+  </div>
+  </div>
+  
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
