7439276c982a39bd6f91c0dd48262972df60c854
rmit-s3734938-vineet-bugtani <49221259+rmit-s3734938-vineet-bugtani@users.noreply.github.com>
2019-10-14 19:57:46 +1100

    test-2

--- testmap.php
+++ testmap.php
@@ -45,7 +45,7 @@ error_reporting(E_ALL); ?>
   </head>
   <body>
 	  <!-- added -->
-<img id="myImg" src="img_pulpit.jpg" alt="The Pulpit Rock" width="304" height="228"> //14/10
+<img id="myImg" src="https://turntable.kagiso.io/images/Screen_Shot_2019-03-26_at_9.55.42_AM.width-800.png" alt="The Pulpit Rock" width="304" height="228"> <!-- //14/10 -->
 <input id="btn" type="button" value="Send email" onclick="myFunction(); alert('Email sent');"/>
 	    <!-- AWS SDK -->
 <script src="aws-sdk-2.548.0.min.js"></script>
@@ -93,7 +93,7 @@ var body_html = `<html>
     <a href='https://aws.amazon.com//pinpoint/'>the Amazon Pinpoint Email API</a> using the
     <a href='https://aws.amazon.com//sdk-for-node-js/'>
       AWS SDK for JavaScript in Node.js</a>.</p>
-<p id="demo">document.getElementById("myImg").src</p> //14/10
+<p id="demo">document.getElementById("myImg").src</p> <!-- //14/10 -->
 </body>
 </html>`;
 
